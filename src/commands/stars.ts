028aa91a60434200f621ee3d3b04f25cc3348569
Chad Carbert <chadcarbert@me.com>
2019-11-06 12:53:07 -0500

    chore: format code with eslint rules

--- src/commands/stars.ts
+++ src/commands/stars.ts
@@ -3,11 +3,13 @@ import axios from 'axios'
 
 export default class Stars extends Command {
   static description = 'show the github stars on a repository'
+
   static args = [{name: 'repository', required: true}]
+
   async run() {
     const {args} = this.parse(Stars)
     const {data: stargazers} = await axios.get(`https://api.github.com/repos/${args.repository}/stargazers`)
-    for (let s of stargazers) {
+    for (const s of stargazers) {
       this.log(s.login)
     }
   }
--- test/commands/stars.test.ts
+++ test/commands/stars.test.ts
@@ -2,25 +2,25 @@ import {expect, test} from '@oclif/test'
 
 describe('stars', () => {
   test
-    .nock('https://api.github.com', api => {
-      api
-        .get('/repos/oclif/oclif/stargazers')
-        .reply(
-          () => ([
-            200,
-            [
-              {login: 'heroku'},
-              {login: 'oclif'},
-              {login: 'javascript'}
-            ]
-          ])
-        )
-    })
-    .stdout()
-    .command(['stars', 'oclif/oclif'])
-    .it('lists stargazers from repo', ctx => {
-      expect(ctx.stdout).to.contain('heroku')
-      expect(ctx.stdout).to.contain('oclif')
-      expect(ctx.stdout).to.contain('javascript')
-    })
+  .nock('https://api.github.com', api => {
+    api
+    .get('/repos/oclif/oclif/stargazers')
+    .reply(
+      () => ([
+        200,
+        [
+          {login: 'heroku'},
+          {login: 'oclif'},
+          {login: 'javascript'},
+        ],
+      ]),
+    )
+  })
+  .stdout()
+  .command(['stars', 'oclif/oclif'])
+  .it('lists stargazers from repo', ctx => {
+    expect(ctx.stdout).to.contain('heroku')
+    expect(ctx.stdout).to.contain('oclif')
+    expect(ctx.stdout).to.contain('javascript')
+  })
 })
